0460875827690d27a1bc7212c0efde7fdc49f112
Dargeo <56662907+Dargeo@users.noreply.github.com>
2020-09-04 16:18:12 -0500

    cambios header y login del pull request

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
