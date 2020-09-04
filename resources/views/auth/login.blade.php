8db5c526c73bac14b6693d5494262f6aaa08f0b0
Dargeo <alejoarve12@gmail.com>
2020-09-03 19:54:56 -0500

    inicio rama login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.header")
+@section("content")
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+            <div class="panel heading">
+                <h1>Acceso a la aplicacion</h1>
+            </div> 
+            </div>
+        </div> 
+    </div>
+@endsection
