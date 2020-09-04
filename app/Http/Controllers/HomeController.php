6a55388a0f42bb73f1741d1e2f9cc85535edd60a
Dargeo <alejoarve12@gmail.com>
2020-09-04 07:16:10 -0500

    login y register con username

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
-class index extends Controllers
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
 {
-    return \view('home')
-}   
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('header');
+    return view('welcome');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
